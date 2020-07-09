69f0b54db88f5a2ee8f5a15cc0993ea5079215cd
bleed101 <42139064+bleed101@users.noreply.github.com>
2020-07-09 23:53:06 +0530

    Adding the "Add to Cart" Functionality

---
+++ section12/v196/models/user.js
@@ -0,0 +1,47 @@
+const mongodb=require('mongodb');
+const getDb=require('../util/database').getDb;
+
+const ObjectId=mongodb.ObjectId;
+
+class User{
+    constructor(username,email,cart,id){
+        this.name=username;
+        this.email=email;
+        this.cart=cart;
+        this._id=id;
+    }
+    
+    save(){
+        const db=getDb();
+        return db.collection('users')
+        .insertOne(this);
+    }
+
+    addToCart(product){
+        // const cartProductIndex=this.cart.items.findIndex(cp=>{
+        //     return cp.productId.toString()===product._id.toString();
+        // });
+        const updatedCart={items:[{productId:new ObjectId(product._id),quantity:1}]};
+        const db=getDb();
+        return db
+        .collection('users')
+        .updateOne(
+            {_id:new ObjectId(this._id)},
+            {$set:{cart:updatedCart}}
+        );
+    }
+    static findByPk(userId){
+        const db=getDb();
+        return db
+        .collection('users')
+        .findOne({_id:new ObjectId(userId)})
+        .then(user=>{
+            console.log(user);
+            return user;
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+    };
+}
+module.exports=User;
