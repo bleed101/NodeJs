b186ea790544e7497202c6da0cd5b29ef6b80684
bleed101 <42139064+bleed101@users.noreply.github.com>
2020-07-18 23:57:01 +0530

    Adding the Request Driven Login Solution

---
+++ section14/v233/app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes= require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    User.findById('5eec6221d98bca36d40c0ab8')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+    .connect('mongodb+srv://Jack:123@ecommerce-ksnge.mongodb.net/shop?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        User.findOne()
+            .then(user => {
+                if (!user) {
+                    const user = new User({
+                        name: 'DD',
+                        email: 'dd@gmail.com',
+                        cart: {
+                            items: []
+                        }
+                    });
+                    user.save();
+                }
+            })
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
