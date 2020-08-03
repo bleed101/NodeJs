8d510e61f6a12c171d56980d72e464d606fd2c42
bleed101 <42139064+bleed101@users.noreply.github.com>
2020-08-03 23:26:58 +0530

    Setup & Basic Validation

---
+++ section18/v290/routes/auth.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { check, body } = require('express-validator/check');
+
+const authController = require('../controllers/auth')
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/login', authController.postLogin);
+
+router.post(
+    '/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            .custom((value, { req }) => {
+                // if (value === 'entender8ahora@gmail.com') {
+                //     throw new Error('This email is forbidden.');
+                // }
+                // return true;
+                return User.findOne({ email: value }).then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            'E-Mail exists already, please pick a different one.'
+                        );
+                    }
+                });
+            }),
+        body(
+            'password',
+            'Password should be alphanumeric and 5 in length')
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password didn\'t match');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
+module.exports = router;
